5764bcb6b547a2ea94beca7743132037447838d9
atanasdim <hi.atanasdim@gmail.com>
2021-06-26 11:39:58 +0100

    Test for Netlify deploy

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
-import App from "./components/App/App";
+import App from "./Components/App/App";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
