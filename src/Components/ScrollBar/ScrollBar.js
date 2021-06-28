589ea65724a336f497aaf09ffe2c764e8456ad60
atanasdim <hi.atanasdim@gmail.com>
2021-06-28 10:39:58 +0100

    Update ScrollBar state and setting scroll values

--- src/Components/ScrollBar/ScrollBar.js
+++ src/Components/ScrollBar/ScrollBar.js
@@ -1,37 +1,44 @@
+import { ControlCameraOutlined } from "@material-ui/icons";
 import React, { useState, useEffect } from "react";
 import "./ScrollBar.scss";
 
 const ScrollBar = ({ children, isOpen }) => {
   const [scrollPercentage, setScrollPercentage] = useState(0);
   const [isScrollable, setIsScrollable] = useState(false);
-
-  let scrollableElem;
-  let scrollTop = 0;
-  let scrollHeight = 0;
-  let offsetHeight = 0;
+  const [scrollableElem, setScrollableElem] = useState(null);
+  const [scrollTop, setScrollTop] = useState(0);
+  const [scrollHeight, setScrollHeight] = useState(0);
+  const [offsetHeight, setOffsetHeight] = useState(0);
 
   // Check if the panel is scrollable to show toggle button at the bottom
   const checkIsScrollable = () => {
+    console.log(scrollHeight, offsetHeight);
     scrollHeight > offsetHeight
       ? setIsScrollable(true)
       : setIsScrollable(false);
   };
 
   const updateScrollValues = () => {
-    scrollTop = scrollableElem.scrollTop;
-    scrollHeight = scrollableElem.scrollHeight;
-    offsetHeight = scrollableElem.offsetHeight;
+    setScrollTop(scrollableElem.scrollTop);
+    setScrollHeight(scrollableElem.scrollHeight);
+    setOffsetHeight(scrollableElem.offsetHeight);
     setScrollPercentage((scrollTop / (scrollHeight - offsetHeight)) * 50);
   };
 
   // This has to use children is the dependency array in order to update the scroll values with the rendered DOM element
   useEffect(() => {
     // Selecting the first child, which is the target element to have the scrollbar attached
-    scrollableElem = document.getElementById("scroll-wrapper").children[0];
-    updateScrollValues();
-    scrollableElem.onscroll = updateScrollValues;
-    checkIsScrollable();
+    setScrollableElem(document.getElementById("scroll-wrapper").children[0]);
+    console.log(scrollableElem);
   }, [children]);
+
+  useEffect(() => {
+    if (scrollableElem) {
+      updateScrollValues();
+      scrollableElem.onscroll = updateScrollValues;
+      checkIsScrollable();
+    }
+  }, [children, isOpen]);
 
   return (
     <div className="scroll-wrapper" id="scroll-wrapper">
