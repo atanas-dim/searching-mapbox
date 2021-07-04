363b42477dad3fa3d6b7d66d3faa8e119798149b
atanasdim <hi.atanasdim@gmail.com>
2021-07-04 23:43:04 +0100

    Resize listener passive

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -31,11 +31,15 @@ function App() {
     // Then we set the value in the --vh custom property to the root of the document
     document.documentElement.style.setProperty("--vh", `${vh}px`);
 
-    window.addEventListener("resize", () => {
-      // We execute the same script as before
-      vh = window.innerHeight * 0.01;
-      document.documentElement.style.setProperty("--vh", `${vh}px`);
-    });
+    window.addEventListener(
+      "resize",
+      () => {
+        // We execute the same script as before
+        vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty("--vh", `${vh}px`);
+      },
+      { passive: true }
+    );
   }, []);
 
   // Set the theme on the DOM element
