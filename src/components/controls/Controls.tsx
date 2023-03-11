86277352b78d8616a6993a7ed77c84d4fa2ced65
Atanas Dimitrov <hi.atanasdim@gmail.com>
2023-03-11 11:59:53 +0000

    Update controls with new icons, refactor and clean geoloc hook

--- src/components/controls/Controls.tsx
+++ src/components/controls/Controls.tsx
@@ -1,39 +1,44 @@
 import React, { FC } from "react";
 import { useMap } from "src/context/MapContext";
 import useGeolocControls, { BtnVariant } from "src/hooks/useGeolocControls";
+import GeolocActiveAwayIcon from "src/components/icons/GeolocActiveAwayIcon";
+import GeolocActiveIcon from "src/components/icons/GeolocActiveIcon";
+import GeolocDisabledIcon from "src/components/icons/GeolocDisabledIcon";
 
 import styles from "./Controls.module.scss";
+import classNames from "classnames";
+import PlusIcon from "../icons/PlusIcon";
+import MinusIcon from "../icons/MinusIcon";
 
 const Controls: FC = () => {
   const { zoomIn, zoomOut, map } = useMap();
 
-  const { handleGeolocClick, isTrackingUserLoc, geolocIsActive, btnVariant } =
-    useGeolocControls(map);
+  const { handleGeolocClick, btnVariant } = useGeolocControls(map);
 
   return (
     <div className={styles.container}>
       <button
         onClick={() => handleGeolocClick()}
-        style={{
-          zIndex: 1000,
-        }}
-        className={
-          btnVariant === BtnVariant.Tracking
-            ? styles.trackingActive
-            : btnVariant === BtnVariant.ActiveCentered
-            ? styles.geolocActive
-            : btnVariant === BtnVariant.ActiveAway
-            ? "" // TODO Add style or replace with icons
-            : ""
-        }
+        className={classNames(
+          styles.geolocBtn,
+          btnVariant === BtnVariant.Tracking && styles.tracking,
+          btnVariant === BtnVariant.ActiveCentered && styles.active,
+          btnVariant === BtnVariant.ActiveAway && styles.active
+        )}
       >
-        📍
+        {btnVariant === BtnVariant.Off ? (
+          <GeolocDisabledIcon />
+        ) : btnVariant === BtnVariant.ActiveAway ? (
+          <GeolocActiveAwayIcon />
+        ) : (
+          <GeolocActiveIcon />
+        )}
       </button>
       <button onClick={() => zoomIn()} style={{ zIndex: 1000 }}>
-        ＋
+        <PlusIcon />
       </button>
       <button onClick={() => zoomOut()} style={{ zIndex: 1000 }}>
-        −
+        <MinusIcon />
       </button>
     </div>
   );
--- src/hooks/useGeolocControls.ts
+++ src/hooks/useGeolocControls.ts
@@ -1,16 +1,5 @@
 import mapboxgl, { GeolocateControl, Map } from "mapbox-gl";
-import { useCallback, useEffect, useRef, useState } from "react";
-
-type GeolocState = {
-  isTrackingUserLoc: boolean;
-  geolocIsActive: boolean;
-  isAtUserLoc: boolean;
-};
-const INITIAL_STATE = {
-  isTrackingUserLoc: false,
-  geolocIsActive: false,
-  isAtUserLoc: false,
-};
+import { useEffect, useState } from "react";
 
 export enum BtnVariant {
   Off,
@@ -21,19 +10,8 @@ export enum BtnVariant {
 
 const useGeolocControls = (map: Map | undefined) => {
   const [geolocControls, setGeolocControls] = useState<GeolocateControl>();
-  const [state, setState] = useState<GeolocState>(INITIAL_STATE);
-
-  const updateState = useCallback((state: Partial<GeolocState>) => {
-    setState((prevState) => {
-      return { ...prevState, ...state };
-    });
-  }, []);
 
   const [btnVariant, setBtnVariant] = useState(BtnVariant.Off);
-
-  const isActive =
-    btnVariant === BtnVariant.ActiveAway ||
-    btnVariant === BtnVariant.ActiveCentered;
 
   useEffect(() => {
     if (!map) return;
@@ -55,55 +33,28 @@ const useGeolocControls = (map: Map | undefined) => {
     if (!geolocControls) return;
 
     const onTrackUserStart = (): void => {
-      console.log("HERE 0", btnVariant);
-
       if (btnVariant === BtnVariant.Off) setBtnVariant(BtnVariant.Tracking);
       if (btnVariant === BtnVariant.ActiveAway)
         setBtnVariant(BtnVariant.ActiveCentered);
-
-      updateState({ isTrackingUserLoc: true, isAtUserLoc: false });
     };
 
     geolocControls.on("trackuserlocationstart", onTrackUserStart);
 
-    const onTrackUserEnd = (event: any): void => {
-      console.log("HERE 1");
-
+    const onTrackUserEnd = (): void => {
       setBtnVariant(BtnVariant.ActiveAway);
-
-      updateState({
-        isTrackingUserLoc: false,
-        geolocIsActive: true,
-        isAtUserLoc: false,
-      });
     };
 
     geolocControls.on("trackuserlocationend", onTrackUserEnd);
 
-    const onGeolocate = (event: any): void => {
-      console.log("HERE 2");
-
+    const onGeolocate = (): void => {
       setBtnVariant(BtnVariant.ActiveCentered);
-
-      updateState({
-        isTrackingUserLoc: false,
-        geolocIsActive: true,
-        isAtUserLoc: true,
-      });
     };
 
     geolocControls.on("geolocate", onGeolocate);
 
-    const onError = (): void => {
-      console.log("HERE 3");
-
-      setBtnVariant(BtnVariant.Off);
-
-      updateState({
-        isTrackingUserLoc: false,
-        geolocIsActive: false,
-        isAtUserLoc: false,
-      });
+    const onError = (error: any): void => {
+      console.error(error.message);
+      setBtnVariant(BtnVariant.Off); // TODO This can be Error state with red colour
     };
 
     geolocControls.on("error", onError);
@@ -112,28 +63,22 @@ const useGeolocControls = (map: Map | undefined) => {
     document.querySelector(".mapboxgl-control-container")?.remove();
 
     return () => {
-      //here clear listeners
-      console.log("CLEAR LISTENERS");
       geolocControls.off("trackuserlocationstart", onTrackUserStart);
       geolocControls.off("trackuserlocationend", onTrackUserEnd);
       geolocControls.off("geolocate", onGeolocate);
       geolocControls.off("error", onError);
     };
-  }, [btnVariant, geolocControls, updateState]);
+  }, [btnVariant, geolocControls]);
 
   const handleGeolocClick = () => {
     geolocControls?.trigger();
 
     if (btnVariant === BtnVariant.ActiveCentered) {
-      console.log("SETTING OFF");
       setBtnVariant(BtnVariant.Off);
-      updateState({ geolocIsActive: false });
     }
   };
 
-  console.log({ btnVariant });
-
-  return { handleGeolocClick, btnVariant, ...state };
+  return { handleGeolocClick, btnVariant };
 };
 
 export default useGeolocControls;
